bfdfe09b1dfbcca9af025c9b0bcc87afce7a418a
Andrew Golovashevich <tankist.scratch.p@gmail.com>
2021-11-24 21:41:06 +0300

    Rewriting to cwapi

--- cw3_kamikadze_bot.py
+++ cw3_kamikadze_bot.py
@@ -1,5 +1,5 @@
 import asyncio
-from datetime import datetime, tzinfo
+from datetime import datetime
 import re
 import sys
 from io import StringIO
@@ -8,6 +8,9 @@ from aiogram import Bot, Dispatcher
 from aiogram.dispatcher.filters import Filter
 from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from aiogram.utils.exceptions import MessageNotModified
+from cwapi import AsyncChatWarsApiClient, Server as CWServer
+from cwapi.requests import CreateAuthCodeRequest, GrantTokenRequest, RequestProfileRequest
+from cwapi.responses import ApiException, ForbiddenError, NoSuchUserError
 from sqlalchemy import Table, Column, MetaData, BigInteger, DateTime, Text, select, and_, update
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
@@ -17,12 +20,10 @@ table = Table(
     "kamikadzes",
     MetaData(),
     Column("user", BigInteger, primary_key=True, unique=True),
-    Column("date", DateTime),
-    Column("attack", BigInteger),
-    Column("name", Text)
+    Column("token", Text),
+    Column("name", Text),
+    Column("atk", BigInteger)
 )
-
-profile_pattern = re.compile(r"(?:^|\n)([^\w\[]\ufe0f?(?:\[\w{2,3}]|)[\w\s]{4,16})\n.+\n.+Атака:\s(\d+)")
 
 
 class KamikadzeDatabase:
@@ -36,24 +37,22 @@ class KamikadzeDatabase:
             await c.execute(CreateTable(table, if_not_exists=True))
         return self
 
-    async def set_user(self, uid: int, date: int, name: str, attack: int):
+    async def set_user(self, uid: int, token: str, name: str, atk: int):
         if not isinstance(uid, int):
             raise TypeError(f"uid must be int (got {type(uid) !r})")
-        if not isinstance(date, datetime):
-            raise TypeError(f"uid must be {datetime.__qualname__} (got {type(date) !r})")
+        if not isinstance(token, str):
+            raise TypeError(f"token must be str (got {type(uid) !r})")
         if not isinstance(name, str):
-            raise TypeError(f"uid must be str (got {type(name) !r})")
-        if not isinstance(attack, int):
-            raise TypeError(f"uid must be int (got {type(attack) !r})")
+            raise TypeError(f"name must be str (got {type(uid) !r})")
+        if not isinstance(atk, str):
+            raise TypeError(f"atk must be int (got {type(uid) !r})")
 
         async with self.__sf.begin() as c:
             for rec in await c.execute(select(table).where(table.c.user == uid)):
-                if rec.date.timestamp() >= date.timestamp():
-                    return False
-                await c.execute(update(table).where(table.c.user == uid).values(date=date, name=name, attack=attack))
+                await c.execute(update(table).where(table.c.user == uid).values(token=token, name=name, atk=atk))
                 return True
-            await c.execute(table.insert().values(user=uid, date=date, name=name, attack=attack))
-        return True
+            await c.execute(table.insert().values(user=uid, token=token, name=name, atk=atk))
+            return True
 
     async def get_user(self, uid: int):
         if not isinstance(uid, int):
@@ -61,21 +60,24 @@ class KamikadzeDatabase:
 
         async with self.__sf.begin() as c:
             for rec in await c.execute(select(table).where(table.c.user == uid)):
-                return rec.name, rec.attack
+                return rec.token, rec.name, rec.atk
         return None
 
     async def get_all_users(self):
         users = []
         async with self.__sf.begin() as c:
             for rec in await c.execute(select(table)):
-                users.append((rec.user, rec.name, rec.attack))
+                users.append((rec.user, rec.name, rec.atk))
         return users
 
 
-class HeroFilter(Filter):
+auth_code_pattern = re.compile("^Code (\d+) to authorize [^.]+. This app will have the access to:")
+
+
+class AuthCodeFilter(Filter):
     @staticmethod
     async def check(msg: Message):
-        return msg.is_forward() and msg.forward_from.id == 265204902 and profile_pattern.search(msg.text) is not None
+        return msg.is_forward() and msg.forward_from.id == 265204902 and auth_code_pattern.search(msg.text) is not None
 
 
 class PrefixCheckFilter(Filter):
@@ -93,13 +95,14 @@ squad_msg_row_pattern = re.compile(r"^\D*(\d+)\D.+tg://user\?id=(\d+)\D")
 
 
 class KamikadzeBot:
-    __slots__ = "__bot", "__dp", "__db", "__current_battle"
+    __slots__ = "__bot", "__dp", "__db", "__current_battle", "__cwapi_client"
 
-    def __init__(self, token, *, database):
+    def __init__(self, token, *, database, cwapi_client):
         self.__bot = Bot(token=token)
         self.__dp = Dispatcher(self.__bot)
         self.__db = database
-        self.__dp.register_message_handler(self.__parse_profile, HeroFilter())
+        self.__cwapi_client = cwapi_client
+        self.__dp.register_message_handler(self.__grant_token, AuthCodeFilter())
         self.__dp.register_message_handler(self.__get_me, commands=["me"])
         self.__dp.register_message_handler(self.__button, commands=["suicide"])
         self.__dp.register_callback_query_handler(self.__add, PrefixCheckFilter("add"))
@@ -114,23 +117,26 @@ class KamikadzeBot:
         asyncio.create_task(self.__reset())
         await self.__dp.start_polling()
 
-    async def __parse_profile(self, msg: Message):
-        match = profile_pattern.search(msg.text)
+    async def __grant_token(self, msg: Message):
+        match = auth_code_pattern.search(msg.text)
         if match is None:
             return
+        try:
+            tr = await self.__cwapi_client.ask(GrantTokenRequest(userId=msg.from_user.id, authCode=match.group(1)))
+        except ApiException as exc:
+            print(exc.raw, file=sys.stderr)
+            return
 
-        name, attack = match.group(1), int(match.group(2))
-        if await self.__db.set_user(msg.from_user.id, msg.forward_date, name, attack):
-            await msg.reply(f"Профиль записан:\n<code>{name} \u2694\ufe0f{attack}</code>", parse_mode="html")
-        else:
-            await msg.reply("Слишком старый профиль")
+        await self.__db.set_user(uid=msg.from_user.id, token=tr.token, name="", atk=0)
+
+        await msg.reply_animation("CgACAgIAAxkBAANqYZ6BWAvAk3tUGcPzDQ-Ghos45yYAAncCAALDRClIzjRfROqXvikiBA", caption=f"Запомнил тебя", parse_mode="html")
 
     async def __get_me(self, msg: Message):
         data = await self.__db.get_user(msg.from_user.id)
         if data is None:
             await msg.reply("Ты слишком сильно любишь жить, чтобы мне была интересна информация про тебя")
         else:
-            await msg.reply(f"<code>{data[0]} \u2694\ufe0f{data[1]}</code>", parse_mode="html")
+            await msg.reply(f"<code>{data[1]} \u2694\ufe0f{data[2]}</code>", parse_mode="html")
 
     async def __reset(self):
         while True:
@@ -154,10 +160,42 @@ class KamikadzeBot:
             nonlocal atk, u, new_rows, self
             u = await self.__db.get_user(query.from_user.id)
             if u is None:
-                await query.message.reply(f"<a href='tg://user?id={query.from_user.id}'>{query.from_user.full_name}</a> скинь свой <a href='https://t.me/share/url?url=/hero'>/hero</a> для начала", parse_mode="html")
+                try:
+                    self.__cwapi_client.ask(CreateAuthCodeRequest(userId=query.from_user.id))
+                except NoSuchUserError:
+                    await query.answer("Слабак")
+                    await query.message.reply(f"<a href='tg://user?id={query.from_user.id}'>{query.from_user.full_name}</a> ты нонейм", parse_mode="html")
+                except ApiException as exc:
+                    print(exc.raw, file=sys.stderr)
+                    return True
+
                 await query.answer("Профиль где?")
+                await query.message.reply(f"<a href='tg://user?id={query.from_user.id}'>{query.from_user.full_name}</a> покажи мне код <u>с банковской карточки</u> который пришёл тебе в игре", parse_mode="html")
                 return True
-            new_rows.write(f"<code>{str(u[1]).rjust(5, ' ')}\u2694\ufe0f</code> <a href='tg://user?id={query.from_user.id}'>{u[0]}</a>\n")
+            try:
+                pr = await self.__cwapi_client.ask(RequestProfileRequest(token=u[0]))
+            except ForbiddenError:
+                await query.answer("Ты пидор")
+                await query.message.reply(f"<a href='tg://user?id={query.from_user.id}'>{query.from_user.full_name}</a> <code>/revoke</code> для пидоров", parse_mode="html")
+                return True
+            except ApiException as exc:
+                print(exc.raw, file=sys.stderr)
+                return True
+
+            if pr.guild is None:
+                name = str(pr.castle)
+            else:
+                if pr.guild.emoji is None:
+                    name = str(pr.castle)
+                else:
+                    name = pr.guild.emoji
+                if pr.guild.tag is not None:
+                    name += f"[{pr.guid.tag}]"
+            name += pr.userName
+
+            await self.__db.set_user(uid=query.from_user.id, token=u[0], name=name, atk=pr.atk)
+
+            new_rows.write(f"<code>{str(pr.atk).rjust(5, ' ')}\u2694\ufe0f</code> <a href='tg://user?id={query.from_user.id}'>{name}</a>\n")
             atk = atk - neg + u[1]
 
         for row in filter(bool, (m.group(2) or "").split("\n")):
@@ -185,12 +223,12 @@ class KamikadzeBot:
 async def amain(argv):
     engine = create_async_engine(f"postgresql+asyncpg://{argv[2]}:{argv[3]}@{argv[4]}:{argv[5]}/{argv[6]}", )
     db = await KamikadzeDatabase.connect(sessionmaker(engine, class_=AsyncSession))
-    await KamikadzeBot(argv[1], database=db).run()
-
+    cwapi_client = AsyncChatWarsApiClient(CWServer.CW3, argv[7], argv[8])
+    await cwapi_client.connect()
+    await KamikadzeBot(argv[1], database=db, cwapi_client=cwapi_client).run()
 
 def main(argv):
     asyncio.get_event_loop().run_until_complete(amain(argv))
-
 
 if __name__ == "__main__":
     exit(main(sys.argv))
--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@ from setuptools import setup
 
 setup(
     name="cw3_kamikadze_bot",
-    py_modules =["cw3_kamikadze_bot"],
+    py_modules=["cw3_kamikadze_bot"],
     version="0.0rc0",
     python_requires=">=3.8",
-    install_requires=["aiogram", "sqlalchemy", "asyncpg"],
+    install_requires=[
+        "aiogram",
+        "sqlalchemy",
+        "asyncpg",
+        "chatwars-api @ git+https://github.com/LandgrafHomyak/cwapi-python.git@v1!2021.11.24b0"
+    ],
     entry_points={
         "console_scripts": [
             "cw3_kamikadze_bot=cw3_kamikadze_bot:main"
