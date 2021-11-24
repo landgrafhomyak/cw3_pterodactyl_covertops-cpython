3afc0cb50c44328468f6c768861f45202cf276ee
Andrew Golovashevich <tankist.scratch.p@gmail.com>
2021-11-25 00:20:08 +0300

    Full setup script

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@ from setuptools import setup
 setup(
     name="cw3_kamikadze_bot",
     py_modules=["cw3_kamikadze_bot"],
-    version="0.0rc0",
-    python_requires=">=3.8",
+    description="Bot for managing secret covert operations in Chat Wars",
+    version="1.0rc0",
+    author="Andrew Golovashevich",
+    url="https://LandgrafHomyak.github.io/cw3_pterpdactyl_covertops-cpython/",
+    download_url="https://github.com/LandgrafHomyak/cw3_pterpdactyl_covertops-cpython/releases/tag/1.0rc0",
+    python_requires=">=3.8, <3.12",
     install_requires=[
         "aiogram",
         "sqlalchemy",
@@ -16,4 +20,21 @@ setup(
             "cw3_kamikadze_bot=cw3_kamikadze_bot:main"
         ]
     },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Plugins",
+        "Framework :: AsyncIO",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: Russian",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Games/Entertainment",
+        "Topic :: Games/Entertainment :: Role-Playing",
+        # "Typing :: Typed"
+    ],
+    license="MIT",
+    license_files=["LICENSE"],
 )
